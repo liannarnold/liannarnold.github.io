05ba096d0ce9ad396fffb6f9c87fa576bdbe01ec
Liane <51239866+liannarnold@users.noreply.github.com>
2025-02-13 17:49:17 -0600

    Updated CORS
    updated vite.config.ts with CORS information and changed the dev server port to 3000.

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    port: 3000,
+    strictPort: true,
+    cors: {
+      origin: ['http://localhost:3000'],
+      methods: ['GET', 'POST'],
+      allowedHeaders: ['Content-Type'],
+    },
+    hmr: {
+      protocol: 'ws',
+      host: 'localhost',
+    },
+  },
 })
